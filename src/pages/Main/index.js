3db630854f5c4760ad0d82c639a90a1b86cbe99c
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-08-01 16:36:44 -0300

    Saving repositories to LocalStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,6 +10,23 @@ export default class Main extends Component {
     loading: false,
   };
 
+  // Carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // Salvaar os dados do localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   };
